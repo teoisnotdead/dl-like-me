6803ed958ae234ce5330edfcbb46c847acb77fb4
TEO <alfredo.ssm@gmail.com>
2024-12-02 16:24:54 -0300

    Se limpian console.log

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -7,9 +7,7 @@ export const Post = {
       const values = [titulo, img, descripcion, likes || 0]
 
       await DB.query(SQL, values)
-      console.log('Post creado con éxito en la base de datos')
     } catch (error) {
-      console.error('Error en el modelo Post.create:', error) 
       throw error
     }
   },
